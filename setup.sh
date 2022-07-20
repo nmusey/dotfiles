4ade0fd1cc9ee4c2b3cd31751004d0053775ba99
Nick Musey <50467771+nmusey@users.noreply.github.com>
2022-07-19 22:10:56 -0700

    Add setup file initial draft

---
+++ setup.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+# Automatic setup script for Unix computers.
+# By Nick Musey
+
+usage="bash setup.sh {os} -- os is one of 'ubuntu', 'macos'"
+
+allOS=("ubuntu", "macos")
+
+# Install programs on Ubuntu
+if [[ "$1" = "ubuntu" ]]; then
+    sudo apt update
+    sudo apt install -y git zsh neovim stow ripgrep fzf exa bat curl
+    sudo apt upgrade -y
+
+    # Docker is fun to install
+    sudo apt install ca-certificates gnupg lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    sudo rm /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+    sudo apt install -y python3-pip 
+elif [[ "$1" = "macos" ]]; then
+    xcode-select --install
+    sudo xcodebuild -license accept
+
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" # Install homebrew
+    brew install git zsh neovim stow ripgrep fzf exa bat curl
+    brew install python
+    brew install --cask mos docker iterm2
+    brew cleanup
+else
+    echo $usage
+    exit 1
+fi
+
+# Install languages
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 16
+
+# Setup termianal envionment
+git clone https://github.com/nmusey/dotfiles
+sudo chsh -s $(which zsh)
+
+cd $HOME/dotfiles
+stow zsh
+stow nvim
+
+# Clean up
+cd $HOME
+echo
+echo
+echo
+echo "Installation is complete."
