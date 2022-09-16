6510b4c82d5e66a278443152d7af7e6db0e5a07e
Nick Musey <nmusey@gmail.com>
2022-09-15 17:07:02 -0700

    Add vim plug to setup.sh

--- setup.sh
+++ setup.sh
@@ -40,11 +40,14 @@ else
     exit 1
 fi
 
-# Install languages
+# Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install 16
+
+# Install vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Setup termianal envionment
 sudo chsh -s $(which zsh)
