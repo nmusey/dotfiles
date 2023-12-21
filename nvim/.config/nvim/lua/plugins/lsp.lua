a421d9450f5e9237e421af7e3d21bbcf8a85dbbe
Nick Musey <nmusey@gmail.com>
2023-12-20 20:02:50 -0800

    Fix leftover merge conflict

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,14 +17,10 @@ local on_lsp_attach = function(args)
     vim.keymap.set('n', 'gr', telescope.lsp_references, {})
     vim.keymap.set('n', '<leader>f', vim.lsp.buf.code_action, {})
     vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, {})
-<<<<<<< HEAD
     vim.keymap.set('n', '<leader>n', vim.lsp.buf.rename, {})
-=======
-    vim.keymap.set('n', '<leader>re', vim.lsp.buf.rename, {})
     vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
     vim.keymap.set('n', '<leader>ep', vim.diagnostic.goto_prev)
     vim.keymap.set('n', '<leader>en', vim.diagnostic.goto_next)
->>>>>>> eaa8203 (Various config changes)
 end
 
 local lsp_capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
