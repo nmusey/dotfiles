6fa0b80ac3f3b4a918b1a8c8797d9bb3772fc9c1
nmusey <nmusey@gmail.com>
2025-03-10 08:48:13 -0700

    Add pywal neovim integration

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -2,10 +2,22 @@ return {
     "sainnhe/everforest",
     "morhetz/gruvbox",
     {
-       "rebelot/kanagawa.nvim",
+        "AlphaTechnolog/pywal.nvim",
+        config = function()
+            local pywal = require('pywal')
+            if pywal then
+                pywal.setup()
+                vim.cmd([[colorscheme pywal]])
+            else
+                vim.cmd([[colorscheme kanagawa]])
+            end
+        end
+    },
+    {
+        "rebelot/kanagawa.nvim",
         lazy = false,
         priority = 1000,
-        config = function() 
+        config = function()
             vim.cmd([[colorscheme kanagawa]])
         end
     },
@@ -13,9 +25,9 @@ return {
         "goolord/alpha-nvim",
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
-          local startify = require("alpha.themes.dashboard")
-          startify.file_icons = "devtools"
-          require("alpha").setup(startify.config)
+            local startify = require("alpha.themes.dashboard")
+            startify.file_icons = "devtools"
+            require("alpha").setup(startify.config)
         end,
     },
 }
