918571b74d3d9f4babcc09c6e8086bf963d99475
Nick Musey <nmusey@gmail.com>
2026-02-14 23:40:26 -0800

    Remove merge conflicts

--- nvim/.config/nvim/lua/plugins/dankcolors.lua
+++ nvim/.config/nvim/lua/plugins/dankcolors.lua
@@ -4,20 +4,14 @@ return {
 		priority = 1000,
 		config = function()
 			require('base16-colorscheme').setup({
-<<<<<<< HEAD
-				base00 = '#101418',
-				base01 = '#101418',
-=======
 				base00 = '#0f1417',
 				base01 = '#0f1417',
->>>>>>> 4890571 (Hyprland updates)
 				base02 = '#5c6370',
 				base03 = '#5c6370',
 				base04 = '#abb2bf',
 				base05 = '#ffffff',
 				base06 = '#ffffff',
 				base07 = '#ffffff',
-<<<<<<< HEAD
 				base08 = '#e05f60',
 				base09 = '#e05f60',
 				base0A = '#a9d8ff',
@@ -26,16 +20,6 @@ return {
 				base0D = '#a9d8ff',
 				base0E = '#457f9e',
 				base0F = '#457f9e',
-=======
-				base08 = '#e05f63',
-				base09 = '#e05f63',
-				base0A = '#98ddff',
-				base0B = '#8ae086',
-				base0C = '#5b7c9f',
-				base0D = '#98ddff',
-				base0E = '#3b8292',
-				base0F = '#3b8292',
->>>>>>> 4890571 (Hyprland updates)
 			})
 
 			vim.api.nvim_set_hl(0, 'Visual', {
@@ -44,7 +28,6 @@ return {
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Statusline', {
-<<<<<<< HEAD
 				bg = '#a9d8ff',
 				fg = '#101418',
 			})
@@ -53,7 +36,6 @@ return {
 
 			vim.api.nvim_set_hl(0, 'Statement', {
 				fg = '#457f9e',
-=======
 				bg = '#98ddff',
 				fg = '#0f1417',
 			})
@@ -62,7 +44,6 @@ return {
 
 			vim.api.nvim_set_hl(0, 'Statement', {
 				fg = '#3b8292',
->>>>>>> 4890571 (Hyprland updates)
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Keyword', { link = 'Statement' })
@@ -70,40 +51,31 @@ return {
 			vim.api.nvim_set_hl(0, 'Conditional', { link = 'Statement' })
 
 			vim.api.nvim_set_hl(0, 'Function', {
-<<<<<<< HEAD
 				fg = '#a9d8ff',
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Macro', {
 				fg = '#a9d8ff',
-=======
 				fg = '#98ddff',
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Macro', {
 				fg = '#98ddff',
->>>>>>> 4890571 (Hyprland updates)
 				italic = true
 			})
 			vim.api.nvim_set_hl(0, '@function.macro', { link = 'Macro' })
 
 			vim.api.nvim_set_hl(0, 'Type', {
-<<<<<<< HEAD
 				fg = '#6279a8',
-=======
 				fg = '#5b7c9f',
->>>>>>> 4890571 (Hyprland updates)
 				bold = true,
 				italic = true
 			})
 			vim.api.nvim_set_hl(0, 'Structure', { link = 'Type' })
 
 			vim.api.nvim_set_hl(0, 'String', {
-<<<<<<< HEAD
 				fg = '#88e086',
-=======
 				fg = '#8ae086',
->>>>>>> 4890571 (Hyprland updates)
 				italic = true
 			})
 
