a09e671400cf02f4e14207083b2d16b2e203166e
Nick Musey <nmusey@gmail.com>
2026-08-25 17:35:42 -0700

    Update hyprland packages

--- nix/common/services/hyprland.nix
+++ nix/common/services/hyprland.nix
@@ -23,6 +23,11 @@
       };
     };
 
+    # ly drops to user before making ly-session.log so extra permissions are needed
+    systemd.tmpfiles.rules = [
+      "f /var/log/ly-session.log 0666 root root -"
+    ];
+
     environment.systemPackages = with pkgs; [
       hyprpaper
       hyprshot
@@ -34,8 +39,19 @@
       bemoji
       waybar
       pywal16
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-hyprland
     ];
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-hyprland
+        xdg-desktop-portal-gtk
+      ];
+
+      config.common.default = [
+        "hyprland"
+        "gtk"
+      ];
+    };
   };
 }
